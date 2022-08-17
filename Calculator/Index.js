ecf8728df30e89bf0fe72cbf7bf05617b65cbb8c
MohamedTKhafagy <111205824+MohamedTKhafagy@users.noreply.github.com>
2022-08-17 04:26:31 +0200

    Add files via upload

---
+++ Calculator/Index.js
@@ -0,0 +1,33 @@
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+/*console.log(buttons);*/
+buttons.map( button => {
+    button.addEventListener('click',(e)=>{
+      /*  console.log("clicked");
+        console.log(e);
+        console.log(e.target);
+        console.log(e.target.innerText);*/
+        switch (e.target.innerText){
+            case "AC":
+                display.innerText=' ';
+                break;
+            case "⌫":
+                if(display.innerText) {
+                    display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+            case "=":
+                try{
+                display.innerText=eval(display.innerText);
+                }
+                catch{
+                    display.innerText = "ERROR!!!!"
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
+
